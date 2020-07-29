74653eeee4d71d06c08bd6cc8c2d73f4a866b6cb
jtyoui <jtyoui@qq.com>
2020-07-29 11:09:13 +0800

    增加shape

--- entity_extraction/model.py
+++ entity_extraction/model.py
@@ -12,14 +12,15 @@ import tensorflow_addons as tfa
 from config import *
 
 
-# tf.config.experimental_run_functions_eagerly(True) # 调试
+# tf.config.experimental_run_functions_eagerly(True)  # 调试
+
 
 class NERModel(tf.keras.Model):
     def __init__(self):
         super().__init__()
         tag_size = int(os.environ['TAG_SIZE'])
         self.embedding = tf.keras.layers.Embedding(int(os.environ['VOCAB_SIZE']), output_dim=int(EMBEDDING_SIZE))
-        self.bi_lst = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units=256, return_sequences=True))
+        self.bi_lst = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units=int(LSTM_SIZE), return_sequences=True))
         self.dense = tf.keras.layers.Dense(units=tag_size)
         self.dropout = tf.keras.layers.Dropout(rate=0.5)
         self.params = tf.Variable(tf.random.uniform(shape=(tag_size, tag_size)))
@@ -27,11 +28,12 @@ class NERModel(tf.keras.Model):
     @tf.function(input_signature=[tf.TensorSpec(shape=(None, int(MAX_WORD_LENGTH)), dtype=tf.int32, name='word'),
                                   tf.TensorSpec(shape=(None, int(MAX_WORD_LENGTH)), dtype=tf.int32, name='label')])
     def call(self, word, label):
-        text_length = tf.math.reduce_sum(tf.cast(tf.math.not_equal(word, 0), dtype=tf.int32), axis=-1)
-        x = self.embedding(word)
-        x = self.dropout(x)
-        x = self.bi_lst(x)
-        x = self.dense(x)
+        text_length = tf.math.reduce_sum(tf.cast(tf.math.not_equal(word, 0), dtype=tf.int32), axis=-1)  # (BATCH_SIZE,)
+        x = self.embedding(word)  # (BATCH_SIZE, MAX_WORD_LENGTH, EMBEDDING_SIZE)
+
+        x = self.dropout(x)  # (BATCH_SIZE, MAX_WORD_LENGTH, EMBEDDING_SIZE)
+        x = self.bi_lst(x)  # (BATCH_SIZE, MAX_WORD_LENGTH, int(LSTM_SIZE) * 2)
+        x = self.dense(x)  # (BATCH_SIZE, MAX_WORD_LENGTH, TAG_SIZE)
         label = tf.convert_to_tensor(label, dtype=tf.int32)
         log_likelihood, self.params = tfa.text.crf_log_likelihood(inputs=x,
                                                                   tag_indices=label,
